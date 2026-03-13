7cdec11fcb6d21f22765afe4d5ecebde8e44ff69
Tony Cook <tony@develop-help.com>
2026-03-14 09:51:26 +1100

    more docs

--- lib/Faster/Maths/CC.pm
+++ lib/Faster/Maths/CC.pm
@@ -195,10 +195,76 @@ Faster::Maths::CC - make mathematically-intense programs faster
 
    # and that's it :)
 
+   # optionally:
+   use Faster::Maths::CC "+float";
+
+   # and maybe
+   no overloading;
+
 =head1 DESCRIPTION
 
 This module attempts to compile some perl code into C code, ideally to
 make it faster.
+
+This works similar in concept to L<Faster::Maths> except instead of
+generating a byte code and interpreting that, C code is generated.
+
+When enabled for a scope with:
+
+  use Faster::Maths::CC;
+
+I<some> ops in range (see below) are translated to C code (this may
+change to C++ for reasons) that tries to behave like the original Perl
+ops, including supporting overloading and preserving the lower bits of
+large integer values.
+
+If you also disable overloading with
+
+  no overloading;
+
+checks for overloading are disabled, only slightly improving
+performance.
+
+You can also request working in floating point:
+
+  use Faster::Maths::CC "+float";
+
+which, when overloading is also disabled, will work in pure floating
+point, similar to L<Faster::Maths>.
+
+For the julia set test case from Faster::Maths this produces
+performance improvements like:
+
+  Improvement   Configuration
+  -----------   -------------
+  20%           Faster::Maths::CC
+  21%           Faster::Maths::CC, no overloading
+  35%           Faster::Maths::CC +float, no overloading
+  27%           original Faster::Maths
+
+All relative to native perl performance.
+
+=head2 HOW IT WORKS
+
+During compilation the peep hook is used to trace the C<op_next> chain
+looking for chains of three or more OPs that are supported by the code
+generation.  Logical OP C<op_other> ops start a new sequence of OPs.
+
+Once a sequence of compatible OPs are found C code is generated for
+them, except that instead of pushing and popping values from the perl
+value stack, local variables are used instead.  The code generated
+currently always works with PADTMPs for results, so there's no new SVs
+created beyond those that already exist, avoiding the possibility of
+leaks.
+
+When code is generated a C<callcompiled> OP is inserted before the
+original OPs, this will call the generated code fragment once the XS
+module is generated, compiled and loaded, but falls back to the
+original OPs if it hasn't been.
+
+We don't want to compile each generated C code fragment separately, so
+they're accumulated until C<CHECK> time when an XS module with the
+generated code is created, compiled and loaded.
 
 =head2 BUGS
 
@@ -206,7 +272,25 @@ make it faster.
 
 =item *
 
-Currently writes generated code to STDOUT mixed with debug data
+OPs supported are very limited
+
+=item *
+
+doesn't handle C<+float> with overloading enabled
+
+=item *
+
+warnings are reported against the C<callcompiled> OP rather than the
+original (eg. "addition") operator, which can be confusing.
+
+=item *
+
+doesn't handle C<PERL_RC_STACK> builds
+
+=item *
+
+only Perl code compiled before C<CHECK> time is scanned and compiled
+to C (this won't be fixed for a while, if at all)
 
 =back
 
@@ -216,7 +300,29 @@ Currently writes generated code to STDOUT mixed with debug data
 
 =item *
 
-Implementation
+cache generated code - currently the code is generated and the XS
+module is created and compiled for each run.  It should be possible to
+skip the last steps when restarting the same program.
+
+=item *
+
+Support more OPs
+
+=item *
+
+support more math related ops, builtin functions, POSIX functions
+
+=item *
+
+support more structural code, like loops, conditionals (requires a
+major re-work)
+
+=item *
+
+compile leaf functions to directly callable code, and call that from
+calling functions.  It may be necessary to delay all code gen to
+C<CHECK> time so that functions defined after their call can be called
+directly.
 
 =back
 
